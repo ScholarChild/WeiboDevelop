ffc9099ef8a72da617fc37242efa20766c6850c6
Zero <963302599@qq.com>
2015-12-02 15:40:03 +0800

    修复单图模式bug
    1.修复了微博单元在单图模式下的尺寸缩放bug

--- WeiboDevelop/MainPageClasses/WeiboCellComponent/ImageContext.m
+++ WeiboDevelop/MainPageClasses/WeiboCellComponent/ImageContext.m
@@ -56,7 +56,6 @@
         for (NSInteger col = 0; col < colCount; col++) {
             
             UIImageView* imgView = [[UIImageView alloc]initWithFrame:CGRectMake(positionInterval*col + weiboCellviewInterval, positionInterval*row + weiboCellviewInterval, imgEdgeLength, imgEdgeLength)];
-            [imgView setBackgroundColor:[UIColor yellowColor]];
             NSURL* imgURL = [NSURL URLWithString:[urlStrArr objectAtIndex:index]];
             [imgView sd_setImageWithURL:imgURL placeholderImage:[UIImage imageNamed:@"def.png"]];
             [self addSubview:imgView];
@@ -74,31 +73,32 @@
 
 - (CGFloat)onePicModeWithURLString:(NSString *)urlString
 {
-//    NSURL* imgURL = [NSURL URLWithString:urlString];
-//    
-//    CGSize imgSize = [ImageSizeDownLoader downloadImageSizeWithURL:imgURL];
-//    CGFloat contextWidth = CGRectGetWidth([[UIScreen mainScreen] bounds]) - weiboCellviewInterval*2.0f;
-//    CGFloat imgPrintWidth = 0;
-//    CGFloat imgPrintHeight = 0;
-//    CGFloat pantograph = 1;
-//    
-//    if (imgSize.width / imgSize.height < 1.05) {
-//        imgPrintWidth = contextWidth * 1/2;
-//    }else {
-//        imgPrintWidth = contextWidth * 2/3;
-//    }
-//    pantograph = imgSize.width / imgPrintWidth;
-//    imgPrintHeight = imgSize.height / pantograph;
-//    
-//    CGRect imgPrintFrame = CGRectMake(weiboCellviewInterval, weiboCellviewInterval, imgPrintWidth, imgPrintHeight);
-//    UIImageView* imgView = [[UIImageView alloc]initWithFrame:imgPrintFrame];
-//    [imgView setBackgroundColor:[UIColor yellowColor]];
-//    [imgView sd_setImageWithURL:imgURL];
-//    [self addSubview:imgView];
-    return 60;
+    NSURL* imgURL = [NSURL URLWithString:urlString];
+    
+    CGSize imgSize = [ImageSizeDownLoader downloadImageSizeWithURL:imgURL];
+    CGFloat contextWidth = CGRectGetWidth([[UIScreen mainScreen] bounds]) - weiboCellviewInterval*2.0f;
+    CGFloat imgPrintWidth = imgSize.width;
+    CGFloat imgPrintHeight = imgSize.height;
+    CGFloat aspectRatio = imgSize.width / imgSize.height;
+    
+    NSAssert((aspectRatio > 0.0f), @"单图加载异常！图片尺寸获取失败");
+    
+    if (aspectRatio >= 2.5f) {
+        imgPrintWidth = contextWidth;
+    }else if (aspectRatio >= 0.9f) {
+        imgPrintWidth = contextWidth * 2.0f / 3.0f;
+    }else {
+        imgPrintWidth = contextWidth * 1.0f / 2.0f;
+    }
+    imgPrintHeight = imgPrintWidth / aspectRatio;
+    
+    CGRect imgPrintFrame = CGRectMake(weiboCellviewInterval, weiboCellviewInterval, imgPrintWidth, imgPrintHeight);
+    UIImageView* imgView = [[UIImageView alloc]initWithFrame:CGRectIntegral(imgPrintFrame)];
+    [imgView setBackgroundColor:[UIColor yellowColor]];
+    [imgView sd_setImageWithURL:imgURL];
+    [self addSubview:imgView];
+    return imgPrintHeight;
 }
-
-//////////////
 
 @end
 
